e9d959914419a688c05dee02a8bb74dc277fdb4f
neek-empty <moki18182@gmail.com>
2025-07-13 17:05:50 +0500

    Added English localization and lang command

--- MetalHeadShell/Program.cs
+++ MetalHeadShell/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,83 +9,93 @@ using System.Threading.Tasks;
 
 namespace MetalHeadShell
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Title = "MetalHeadShell";
-            Console.ForegroundColor = ConsoleColor.Green;
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "MetalHeadShell";
+			Console.ForegroundColor = ConsoleColor.Green;
+
+            ResourceManager resMan = new ResourceManager("MetalHeadShell.Strings", typeof(Program).Assembly);
+            CultureInfo currentCulture = CultureInfo.CurrentCulture;
+
 
             Console.WriteLine("Добро пожаловать в MetalHeadShell");
-            Console.WriteLine("Напиши help чтобы получить первую помощь или clear чтобы нахуй всё очистить");
+			Console.WriteLine("Напиши help чтобы получить первую помощь или clear чтобы нахуй всё очистить");
+            Console.WriteLine("To change language to English type: lang en");
+            Console.WriteLine("Чтобы сменить язык на русский, напиши: lang ru");
 
-            while (true)
-            {
-                Console.Write(">> ");
+			while (true)
+			{
+				Console.Write(">> ");
+				string input = Console.ReadLine();
 
-                string input = Console.ReadLine();
-
-                switch (input.ToLower())
+                if (input.StartsWith("lang "))
                 {
-                    case "help":
-                        Console.WriteLine("ПЕРВАЯ ПОМОЩЬ RIGHT NOW");
-                        Console.WriteLine("ПОЛНЫЙ СПИСОК КОМАНД ДОСТУПЕН В README.MD");
-                        Console.WriteLine("1.help: то что ты сейчас видишь");
-                        Console.WriteLine("2.hello: поздороваться с тобой");
-                        Console.WriteLine("3.time: подсказать время и дату");
-                        Console.WriteLine("4.clear: очистить констоль");
-                        Console.WriteLine("5.exit: выйти из этого пиздеца (в окно)");
-                        break;
-                    case "hello":
-                        Console.WriteLine("Здарова, братюня!");
-                        break;
-                    case "time":
-                        Console.WriteLine(DateTime.Now);
-                        break;
-                    case "clear":
-                        Console.Clear();
-                        break;
-                    case "notepad":
-                        Console.WriteLine("Аткрываю блокнот...");
-                        Process.Start("notepad.exe");
-                        break;
-                    case "notepad++":
-                        Console.WriteLine("Аткрываю notepad++");
-                        Process.Start(@"C:\Program Files\Notepad++\notepad++.exe");
-                        break;
-                    case "flstudio":
-                        Console.WriteLine("Делай музло, брат");
-                        Console.WriteLine("Аткрываю FL Studio...");
-                        Process.Start(@"C:\Program Files\Image-Line\FL Studio 2024\FL64.exe");
-                        break;
-                    case "vscode":
-                        Console.WriteLine("Программируй, братан");
-                        Console.WriteLine("Аткрываю Visual Studio Code...");
-                        Process.Start(@"C:\Users\Maksim Abdulin\AppData\Local\Programs\Microsoft VS Code\Code.exe");
-                        break;
-                    case "unity":
-                        Console.WriteLine("Юнити, так юнити, брат");
-                        Console.WriteLine("Аткрываю Unity Hub...");
-                        Process.Start(@"C:\Program Files\Unity Hub\Unity Hub.exe");
-                        break;
-                    case "amplitube":
-                        Console.WriteLine("Играй, брат");
-                        Console.WriteLine("Аткрываю Amplitube...");
-                        Process.Start(@"C:\Program Files\IK Multimedia\AmpliTube 5\AmpliTube 5.exe");
-                        break;
-                    case "open flprojects":
-                        Console.WriteLine("Аткрываю папку с fl проэктами...");
-                        Process.Start("explorer.exe", @"D:\Documents\fl_projects");
-                        break;
-                    case "exit":
-                        Console.WriteLine("Пока, братишка.");
-                        return;
-                    default:
-                        Console.WriteLine("Неизвестная команда.");
-                        break;
+                    string langCode = input.Split(' ')[1];
+                    try
+                    {
+                        currentCulture = new CultureInfo(langCode);
+                        Console.WriteLine(resMan.GetString("LangSet", currentCulture));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Unsupported language code");
+                    }
                 }
-            }
-
-        }
-    }
+				else
+				{
+						switch (input.ToLower())
+					{
+						case "help":
+							Console.WriteLine(resMan.GetString("help", currentCulture));
+							break;
+						case "hello":
+							Console.WriteLine(resMan.GetString("hello", currentCulture));
+							break;
+						case "time":
+							Console.WriteLine(DateTime.Now);
+							break;
+						case "clear":
+							Console.Clear();
+							break;
+						case "notepad":
+							Console.WriteLine(resMan.GetString("notepad", currentCulture));
+							Process.Start("notepad.exe");
+							break;
+						case "notepad++":
+							Console.WriteLine(resMan.GetString("notepad__", currentCulture));
+							Process.Start(@"C:\Program Files\Notepad++\notepad++.exe");
+							break;
+						case "flstudio":
+							Console.WriteLine(resMan.GetString("flstudio", currentCulture));
+							Process.Start(@"C:\Program Files\Image-Line\FL Studio 2024\FL64.exe");
+							break;
+						case "vscode":
+							Console.WriteLine(resMan.GetString("vscode", currentCulture));
+							Process.Start(@"C:\Users\Maksim Abdulin\AppData\Local\Programs\Microsoft VS Code\Code.exe");
+							break;
+						case "unity":
+							Console.WriteLine(resMan.GetString("unity", currentCulture));
+							Process.Start(@"C:\Program Files\Unity Hub\Unity Hub.exe");
+							break;
+						case "amplitube":
+							Console.WriteLine(resMan.GetString("amplitube", currentCulture));
+							Process.Start(@"C:\Program Files\IK Multimedia\AmpliTube 5\AmpliTube 5.exe");
+							break;
+						case "open flprojects":
+							Console.WriteLine(resMan.GetString("flprojects", currentCulture));
+							Process.Start("explorer.exe", @"D:\Documents\fl_projects");
+							break;
+						case "exit":
+							Console.WriteLine(resMan.GetString("exit", currentCulture));
+							return;
+						default:
+							Console.WriteLine(resMan.GetString("unknowncommand", currentCulture));
+							break;
+					}
+				}	
+			}
+		}
+	}
 }
